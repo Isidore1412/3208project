4499a878878c67dc0df8776e9284ba330e50ca2c
Isidore1412 <isidore1412@gmail.com>
2021-10-21 21:20:07 +0800

    Update connect_test.php

--- src/connect_test.php
+++ src/connect_test.php
@@ -10,8 +10,8 @@
         CREATE TABLE IF NOT EXISTS "user" (
         username TEXT PRIMARY KEY NOT NULL UNIQUE,
         email CHAR(50) NOT NULL,
-        password CHAR(16) NOT NULL);
-        )
+        password CHAR(16) NOT NULL;
+        );
     EOF;
     $ret = pg_query($db, $sql);
 
