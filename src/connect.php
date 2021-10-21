43f7f78a23a570e5018320fb5d9b21a479fb3344
Isidore1412 <isidore1412@gmail.com>
2021-10-21 16:14:48 +0800

    1

---
+++ src/connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "mongo";
+$username = "php";
+$password = "php";
+
+
+// Check connection
+ //if (!$manager) {
+   //die("Connection failed: ");
+   //}
+echo "Mongo connected successfully! <br>";
+echo "Server Info: ";
+   
+
+?>
