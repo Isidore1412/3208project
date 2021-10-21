7a9db2007be8b7eaf7545c3672f669321c40a45a
Isidore1412 <isidore1412@gmail.com>
2021-10-22 02:12:51 +0800

    1

--- src/index.php
+++ src/index.php
@@ -8,10 +8,6 @@
     <?php
         $conn  =  "host=postgres port=5432 dbname=myphp user=root password=root123" ; 
         $db = pg_connect($conn);
-        #if (!$db) 
-            #echo "db can not connect";
-        #else 
-            #echo "db connect succeed!";
 
         $create_user =<<<EOF
             CREATE TABLE IF NOT EXISTS "user" (
