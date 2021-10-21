90af4e0732d20f2d96b43fdf91186d0feaffd943
Isidore1412 <isidore1412@gmail.com>
2021-10-21 20:58:44 +0800

    Update connect_test.php

--- src/connect_test.php
+++ src/connect_test.php
@@ -7,14 +7,14 @@
         echo "db connect succeed!";
 
     $sql =<<<EOF
-        CREATE TABLE USER
-        (USERNAME TEXT PRIMARY KEY NOT NULL,
+        CREATE TABLE USER(
+        USERNAME TEXT PRIMARY KEY NOT NULL,
         EMAIL CHAR(50) NOT NULL,
         PASSWORD CHAR(16) NOT NULL);
     EOF;
 
     $ret = pg_query($db, $sql);
-    
+
     if(!$ret){
         echo pg_last_error($db);
         pg_close($db);
